d8c8e72598d6600000c2fad9bdd8307bbc613200
Enzo Vasconcelos <enzo.vasconcelos@ccc.ufcg.edu.br>
2026-03-04 11:51:29 -0300

    basic version of template

---
+++ components/util.js
@@ -0,0 +1,3 @@
+export const importModels = () => {
+    return "";
+};
--- template/index.js
+++ template/index.js
@@ -1,36 +1,105 @@
 import { File, Text } from '@asyncapi/generator-react-sdk';
-import { importModels } from '../../components/util';
+//import { importModels } from '../../components/util';
 
 function toPascalCase(name) {
-    return name.replace(' ', '');
+    let firstLetter = name[0];
+    return name.replace(firstLetter, firstLetter.toUpperCase());
 } 
 
+function getMethodName(channelName) {
+    let out = channelName.replaceAll("{", "");
+    out = out.replaceAll("}", "");
+    out = out.replaceAll("/", "");
+    return out;
+}
+
+function getParametersList(parameters) {
+    let out = "";
+    for(let paramName in parameters) {
+        out = toPascalCase(parameters[paramName].schema().type());
+        out += " " + paramName;
+    }
+    return out;
+}
+
 function getChannelWrappers(asyncapi, params) {
+    console.log(params);
     let channelWrappers = []; 
     let channelEntries = Object.keys(asyncapi.channels()).length ? Object.entries(asyncapi.channels()) : [];
     console.log(channelEntries);
-    /*channelEntries.map(([channelName, channel]) => {
-        
-    });*/
+    let out = "";
+    for(let [channelName, channel] of channelEntries) {
+        console.log(channelName);
+        console.log(channel);
+        const methodName = getMethodName(channelName);
+        let parametersList = getParametersList(channel.parameters());
+        if(channel.subscribe()) {
+            out += `
+    public Subscription subscribeOn${methodName}(${parametersList}) {
+        if(!isConnected()) {
+            throw new Exception("Client not connected");
+        }
+        String subject = "${channelName}";
+        this.connection.subscribe(subject);
+    }
+`;
+            console.log(`Esse channel ${channelName} é do tipo subscribe`);
+        } else if(channel.publish()) {
+            console.log(`Esse channel ${channelName} é do tipo publish`);
+            let argumentsList = parametersList;
+            argumentsList += parametersList ? ", " : "";
+            argumentsList += "Byte[] message";
+            out += `
+    public void publishOn${methodName}(${argumentsList}) {
+        if(!isConnected()) {
+            throw new Exception("Client not connected");
+        }
+        String subject = "${channelName}";
+        this.connection.publish(subject, message);
+    }            
+`;
+        }
+    }
+    channelWrappers = channelEntries.map(([channelName, channel]) => {
+        const publishMessage = channel.publish() ? channel.publish().message(0) : undefined;
+        const subscribeMessage = channel.subscribe() ? channel.subscribe().message(0) : undefined;
+        const channelDescription = channel.description();
+        const channelParameters = channel.parameters(); 
+        if(publishMessage) {
+            console.log('--- publish ---');
+        } else if(subscribeMessage) {
+            console.log('--- subscribe ---');
+
+        }
+        console.log(`publish msg: ${publishMessage}`);
+        console.log(`subscribe msg: ${subscribeMessage}`);
+        console.log(`description: ${channelDescription}`);
+        console.log(`parameters: ${channelParameters}`);
+        for(let parameterName in channelParameters) {
+            console.log('parameter name: ', parameterName);
+            console.log(channelParameters[parameterName].schema().type());
+        }
+    });
+    return out;
 }
 
 function formatClassName(title) {
-    return `${toPascalCase(title)}Client.java`;
+    return `${title.replace(" ", "")}Client.java`;
 }
 
 export default function ({ asyncapi, params }) {
   const className = formatClassName(asyncapi.info().title());
   return (
     <File name={className}>
-`
-package asyncapi.nats.client;
+{`
+// TODO: define your package
+// package asyncapi.nats.client;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.time.Duration;
 
-${importModels().join('\n')};
 import asyncapi.nats.models.*;
 
 import io.nats.client.Connection;
@@ -38,7 +107,7 @@ import io.nats.client.Message;
 import io.nats.client.Nats;
 import io.nats.client.Subscription;
 
-public class ${className}Client {
+public class ${asyncapi.info().title().replace(" ", "")}Client {
     
     private Connection connection;    
 
@@ -47,13 +116,19 @@ public class ${className}Client {
     }
 
     public void connect(String url) {
-        connection = Nats.connect(url)
+        connection = Nats.connect(url);
     }
-
-    ${getChannelWrappers(asyncapi, params).join('\n')}
-
+    
+    public void disconnect() {
+        this.connection.close();
+    }
+    
+    public boolean isConnected() {
+        return this.connection != null && this.connection.getStatus() == Connection.Status.CONNECTED;
+    }
+    ${getChannelWrappers(asyncapi, params)}
 }
-`
+`}
     </File>
   );
 }
